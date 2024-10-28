610f8168fcc2b43e07bc5d175674876b6702e966
Kengo <83492993+KengoKato02@users.noreply.github.com>
2024-10-28 10:51:26 +0100

    feature: updated with new tech stack

--- apps/api/src/db/config.js
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export const config = {
-  isProduction: process.env.NODE_ENV === 'production',
-  mongodbAtlasUri: process.env.MONGODB_URI,
-  mongodbLocalUri: process.env.MONGODB_LOCAL_URI
-};
