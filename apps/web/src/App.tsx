40b13b2f0ac4451a0490bb329d7d558cb51f5f3d
Kengo <83492993+KengoKato02@users.noreply.github.com>
2024-11-04 09:44:44 +0100

    router cleanup

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,29 +1,11 @@
-// App.tsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import routes from './routes/Routes';
-import ProtectedRoute from './routes/ProtectedRoute';
+import { BrowserRouter as Router, Routes } from "react-router-dom";
+import RoutesComponent from './Routes';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Routes>
-        {routes.map((route) => (
-          route.protected ? (
-            <Route
-              key={route.path}
-              path={route.path}
-              element={<ProtectedRoute><route.component /></ProtectedRoute>}
-            />
-          ) : (
-            <Route
-              key={route.path}
-              path={route.path}
-              element={<route.component />}
-            />
-          )
-        ))}
-      </Routes>
+      <RoutesComponent />
     </Router>
   );
 };
