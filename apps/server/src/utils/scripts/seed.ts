580634dc1d21a7999daaf239194b86d05a1b8a37
Kengo <83492993+KengoKato02@users.noreply.github.com>
2024-11-12 14:43:11 +0100

    db populated

--- apps/server/src/utils/scripts/seed.ts
+++ apps/server/src/utils/scripts/seed.ts
@@ -4,41 +4,6 @@ import { faker } from "@faker-js/faker";
 const prisma = new PrismaClient();
 
 async function main() {
-  // Create Users
-  for (let i = 0; i < 50; i++) {
-    await prisma.user.create({
-      data: {
-        firstname: faker.name.firstName(),
-        lastname: faker.name.lastName(),
-        email: faker.internet.email(),
-        password: faker.internet.password(),
-        phone_number: faker.phone.number(),
-        bio: faker.lorem.sentence(),
-        instrument: faker.helpers.arrayElement([
-         'Violin', 'Viola', 'Cello', 'Double Bass', 
-         'Flute', 'Oboe', 'Clarinet', 'Bassoon', 'French Horn', 
-        ]),
-      },
-    });
-  }
-
-  // Create Ensembles
-  for (let i = 0; i < 10; i++) {
-    await prisma.ensemble.create({
-      data: {
-        name: faker.company.name(),
-        location: {
-          type: 'Point',
-          coordinates: [parseFloat(faker.address.longitude()), parseFloat(faker.address.latitude())],
-        },
-        hosts: [faker.internet.email()],
-        open_positions: [faker.music.genre()],
-        is_active: faker.datatype.boolean(),
-        member_ids: Array.from({ length: 5 }, () => faker.database.mongodbObjectId()),
-      },
-    });
-  }
-
   // Create Posts
   for (let i = 0; i < 20; i++) {
     await prisma.post.create({
@@ -46,10 +11,10 @@ async function main() {
         ensemble_id: faker.database.mongodbObjectId(),
         title: faker.lorem.sentence(),
         description: faker.lorem.paragraph(),
-        website: faker.internet.url(),
+        website_url: faker.internet.url(),
         type: faker.helpers.arrayElement(['recruitment', 'event']),
         author_id: faker.database.mongodbObjectId(),
-        createdAt: faker.date.recent(),
+        created_at: faker.date.recent(),
       },
     });
   }
@@ -60,8 +25,8 @@ async function main() {
       data: {
         searching_user_id: faker.database.mongodbObjectId(),
         matched_user_id: faker.database.mongodbObjectId(),
-        matchedAt: faker.date.recent(),
-        match_status: faker.helpers.arrayElement(['messaged', 'confirmed']),
+        matched_at: faker.date.recent(),
+        match_status: faker.helpers.arrayElement(['new', 'messaged', 'joined']),
       },
     });
   }
