88ff907315f756f21e7d376a8c630dae2541ae32
Kengo <83492993+KengoKato02@users.noreply.github.com>
2024-11-13 21:22:22 +0100

    init commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,29 @@
 module.exports = {
-  extends: ['extreme'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module'
   },
+  plugins: ['@typescript-eslint', 'react-hooks', 'react-refresh'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  root: true,
   env: {
     node: true,
+    jest: true,
     browser: true,
-    es2021: true,
-  },
-  rules: {
-    'n/no-process-env': 'off',
-    'import-x/extensions': 'off',
-    'import-x/order': 'off',
-    'max-statments': 'off',
+    es2021: true
   },
   ignorePatterns: ['node_modules', 'dist'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  }
 };
