e5607c1609a9a7563a4f50fbd935afed86f73d68
Patrik Hoferica <p.hoferica@outlook.com>
2024-11-19 13:48:50 +0100

    route protection

--- apps/web/src/Routes.tsx
+++ apps/web/src/Routes.tsx
@@ -1,35 +1,31 @@
 import React from "react";
-import { Route, Navigate, Routes } from "react-router-dom";
-//import { useAuth } from "./context/AuthContext";
+import { Route, Routes } from "react-router-dom";
 import Login from "./pages/unauthenticated/auth/Login";
 import Register from "./pages/unauthenticated/auth/Register";
 import Home from "./pages/authenticated/Home";
 import Profile from "./pages/authenticated/Profile";
-import { useSelector } from "react-redux";
-import { RootState } from "./redux/store.ts";
+import PrivateRoute from "./PrivateRoute.tsx";
 
 const RoutesComponent: React.FC = () => {
-  const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
-
   const routes = [
     {
       path: "/login",
-      component: Login,
+      component: <Login />,
       protected: false,
     },
     {
       path: "/register",
-      component: Register,
+      component: <Register />,
       protected: false,
     },
     {
       path: "/home",
-      component: Home,
+      component: <Home />,
       protected: true,
     },
     {
       path: "/profile",
-      component: Profile,
+      component: <Profile />,
       protected: true,
     },
   ];
@@ -41,7 +37,11 @@ const RoutesComponent: React.FC = () => {
           key={route.path}
           path={route.path}
           element={
-            route.protected && !isAuthenticated ? <Navigate to="/login" /> : <route.component />
+            <PrivateRoute
+              element={route.component}
+              redirectTo="/login"
+              protected={route.protected}
+            />
           }
         />
       ))}
