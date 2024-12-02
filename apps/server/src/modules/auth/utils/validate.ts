6b7f40b21b719df664ed5a99cacb355e5ceeef7e
Kengo <83492993+KengoKato02@users.noreply.github.com>
2024-12-02 14:03:21 +0100

    done

---
+++ apps/server/src/modules/auth/utils/validate.ts
@@ -0,0 +1,11 @@
+const isValidEmail = (email: string): boolean => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
+const isValidPassword = (password: string): boolean => {
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+  return passwordRegex.test(password);
+};
+
+export { isValidEmail, isValidPassword };
