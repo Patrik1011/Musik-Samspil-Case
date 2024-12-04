49e2b62ddc3a36375394af7ff6a17af6f9caef55
Patrik Hoferica <p.hoferica@outlook.com>
2024-12-04 23:04:25 +0100

    eslint fix

--- apps/web/src/utils/validateUrl.ts
+++ apps/web/src/utils/validateUrl.ts
@@ -1,23 +1,19 @@
 export const formatAndValidateURL = (website: string): string | null => {
   try {
-    let formattedURL = website.replace(/^www\./, "");
+    let formattedUrl = website.replace(/^www\./, "");
 
-    formattedURL =
-      formattedURL.startsWith("http://") || formattedURL.startsWith("https://")
-        ? formattedURL
-        : `https://${formattedURL}`;
+    formattedUrl =
+      formattedUrl.startsWith("http://") || formattedUrl.startsWith("https://")
+        ? formattedUrl
+        : `https://${formattedUrl}`;
 
-    const validatedUrl = new URL(formattedURL);
+    const validatedUrl = new URL(formattedUrl);
 
-    if (
-      validatedUrl.protocol === "http:" ||
-      validatedUrl.protocol === "https:"
-    ) {
+    if (validatedUrl.protocol === "http:" || validatedUrl.protocol === "https:") {
       return validatedUrl.href;
-    } else {
-      console.error("Invalid protocol:", validatedUrl.protocol);
-      return null;
     }
+    console.error("Invalid protocol:", validatedUrl.protocol);
+    return null;
   } catch (error) {
     console.error("Invalid URL:", error);
     return null;
