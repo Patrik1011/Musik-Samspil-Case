f52ced6568738e3de5c90c4eb38689990815fd58
Patrik Hoferica <p.hoferica@outlook.com>
2025-01-12 15:05:25 +0100

    commit 1 - refactoring
    commit 2 - post service
    
    commit 3 - post controller
    
    Squashed 3 commits into one

--- apps/server/src/modules/ensemble/ensemble.controller.ts
+++ apps/server/src/modules/ensemble/ensemble.controller.ts
@@ -16,31 +16,9 @@ import { CreateEnsembleDto } from "./dto/create-ensemble.dto";
 import { UpdateEnsembleDto } from "./dto/update-ensemble.dto";
 import { AuthenticatedRequest } from "../../utils/interfaces/AuthenticatedRequest";
 
-@Controller("ensemble")
-@ApiTags("ensemble")
-export class EnsembleController {
-  constructor(private readonly ensembleService: EnsembleService) {}
 
-  @Get("hosted")
-  @UseGuards(JwtAuthGuard)
-  @ApiOkResponse()
-  async getHostedEnsembles(@Request() req: AuthenticatedRequest) {
-    return this.ensembleService.findUserHostedEnsembles(req.user._id.toString());
-  }
 
-  @Get(":id")
-  @UseGuards(JwtAuthGuard)
-  @ApiOkResponse()
-  async getEnsemble(@Param("id") id: string, @Request() req: AuthenticatedRequest) {
-    return this.ensembleService.findOne(id, req.user._id.toString());
-  }
-
-  @Post()
-  @UseGuards(JwtAuthGuard)
-  @ApiOkResponse()
-  async create(@Request() req: AuthenticatedRequest, @Body() createEnsembleDto: CreateEnsembleDto) {
-    return this.ensembleService.createWithHost(createEnsembleDto, req.user._id.toString());
-  }
+  
 
   @Put(":id")
   @UseGuards(JwtAuthGuard)
